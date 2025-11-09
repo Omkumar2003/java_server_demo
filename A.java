56839fca0f23a7be93ddaec0ed554c15c9497aa0
Om Kumar <114673166+Omkumar2003@users.noreply.github.com>
2025-11-09 19:56:53 +0530

    Implement HTTPS server with SSL configuration
    This SSL will give a warning because a real SSL needs to be from a certified organisation

--- A.java
+++ A.java
@@ -1,33 +1,61 @@
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpExchange;
-import java.io.IOException;
-import java.io.OutputStream;
+import com.sun.net.httpserver.*;
+import javax.net.ssl.*;
+import java.io.*;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
 
 public class A {
-    public static void main(String[] args) throws IOException {
-        
-        // HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        HttpServer server = HttpServer.create(new InetSocketAddress("192.168.1.4", 8000), 0);
-        
-    
+    public static void main(String[] args) throws Exception {
+
+        // Load the keystore
+        char[] password = "password".toCharArray();
+        KeyStore ks = KeyStore.getInstance("JKS");
+        FileInputStream fis = new FileInputStream("keystore.jks");
+        ks.load(fis, password);
+
+        // Setup key manager factory
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, password);
+
+        // Setup trust manager factory
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(ks);
+
+        // Setup SSL context
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+        // Create HTTPS server
+        HttpsServer server = HttpsServer.create(new InetSocketAddress("192.168.1.4", 8000), 0);
+        server.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+            public void configure(HttpsParameters params) {
+                try {
+                    SSLContext c = getSSLContext();
+                    SSLEngine engine = c.createSSLEngine();
+                    params.setNeedClientAuth(false);
+                    params.setCipherSuites(engine.getEnabledCipherSuites());
+                    params.setProtocols(engine.getEnabledProtocols());
+                    params.setSSLParameters(c.getDefaultSSLParameters());
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        // Create a context
         server.createContext("/", new MyHandler());
-        
-        // Start the server
-        server.setExecutor(null); // default executor
+        server.setExecutor(null);
         server.start();
-        System.out.println("Server started at http://localhost:8000");
+
+        System.out.println("HTTPS server started at https://192.168.1.4:8000");
     }
 
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             String response = "<html><body><h1>Hello World</h1></body></html>";
-            
             exchange.getResponseHeaders().add("Content-Type", "text/html; charset=UTF-8");
             exchange.sendResponseHeaders(200, response.getBytes().length);
-            
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
